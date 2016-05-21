f37c7c69121ee5e2637926aaa4e5af744b32abdd
Jose Garcia <jgarciaquiel@gmail.com>
2016-05-21 11:06:46 -0500

    Add files via upload
    This code converts Fahrenheit tempeture to Celsus and Kelvin, basicaly a test for functions.

---
+++ tempetureconvertor.py
@@ -0,0 +1,15 @@
+#Fahrenheit to Celsus  and Kelvin Convertor
+
+def f2cConveror (t):
+	#print "Converting %.2f Fahrenheit to Celsus" % t
+	c = (t - 32) * (5.0 / 9.0)
+	return c
+
+def c2kConvertor (t):
+	#print "Converting %.2f Celsus to Kelvin" % t testing propuse print.
+	k = (t + 273.15)
+	return k
+
+print "\nSo want to know something about tempeture?\nFahrenheit is not a popular unit to messuere the tempeture\nFor example:"
+fah = input("Enter some F tempeture: ")	
+print "That tempetue %.2f Fahrenheit, becomes:\n%.2f Celsus and %.2f Kelvin, those are more common ways to messuere the tempeture" % (fah, f2cConveror(fah), c2kConvertor(f2cConveror(fah)))
