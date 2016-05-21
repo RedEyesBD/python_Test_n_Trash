a642585ec094f00c2b0bc2a2b8f4e12c786dcf2c
Jose Garcia <jgarciaquiel@gmail.com>
2016-05-21 12:12:31 -0500

    Adding Author, minor changes
    Giving me credit and correcting some erros with the variables, i use spanish names so i translate them

--- area.py
+++ area.py
@@ -1,16 +1,17 @@
 # Area of a Triangle and a Rectangle
+# Author: Jose Garcia - 23.5.12.12 9.6 25.15.21 3.1.14 18.5.1.4 20.8.9.19 25.15.21 1.18.5 19.13.1.18.20
 
 from sys import argv
 
 script, base, side = argv #values as a string, need to convert them first
 
 print "\tAreas\n\n**Triangle**\n"
-area = (float(base) * float(side))*0.5
+area = (float(base) * float(side)) * 0.5 #converting string to float
 print "\n**Total Area**\n%.2f" % area + " square mts"
 
 print "\tAreas\n\n**Rectangle**\n"
 base = float(raw_input("Input the base: ")) #converting string to float
-lado = float(raw_input("Input the side: "))
+side = float(raw_input("Input the side: "))
 
 area = base * side
 
